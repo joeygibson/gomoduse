bef58f31e5f1b72cfb31159974c5226de110ce5a
Joey Gibson <joegibso@cisco.com>
2019-03-26 08:48:03 -0400

    initial commit

---
+++ cmd/gomoduse/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/joeygibson/gomodexample/cmd/gomodexample"
+
+func main() {
+	gomodexample.PrintInRed("I hope i'm in red")
+	gomodexample.PrintInGreen("I hope i'm in green")
+	gomodexample.PrintInBlue("I hope i'm in blue")
+	gomodexample.PrintInYellow("I hope i'm in yellow")
+	gomodexample.PrintInCyan("I hope i'm in cyan")
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/joeygibson/gomoduse
+
+replace github.com/joeygibson/gomodexample => ../gomodexample
+
+go 1.12
+
+require github.com/joeygibson/gomodexample v1.0.0
